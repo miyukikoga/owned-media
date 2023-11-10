0cce2d82e3ab0e6cde7a461d951f7f83927eb975
miyukikoga <>
2023-11-10 21:32:25 +0900

    アイコンを白くする

--- src/components/molecules/HeaderMenu/HeaderMenu.tsx
+++ src/components/molecules/HeaderMenu/HeaderMenu.tsx
@@ -7,7 +7,7 @@ export const HeaderMenu = ({}: Props): JSX.Element => {
   return (
     <div>
       <AppLink href="/profile">
-        <BsPersonCircle color="grey" size="2em" />
+        <BsPersonCircle size="2em" />
       </AppLink>
     </div>
   );
